9af6aec57705a415b710b2e79e29f51c8e089819
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-12-06 16:42:40 +0100

    add @vueuse/core dependency and update App.vue to include ColumnExpand component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <ColumnExpand></ColumnExpand>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import ColumnExpand from './components/ColumnExpand.vue';
+
   //
 </script>
